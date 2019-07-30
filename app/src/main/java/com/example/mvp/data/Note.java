41be8fe8e9d84e6c2ec37a2de67687db665e5f3f
myatthuko <nymyatthuko@gmail.com>
2019-07-30 10:19:30 -0400

    add a note pojo

---
+++ app/src/main/java/com/example/mvp/data/Note.java
@@ -0,0 +1,28 @@
+package com.example.mvp.data;
+
+public class Note {
+
+    String title;
+    String subtitle;
+
+    public Note(String title, String subtitle) {
+        this.title = title;
+        this.subtitle = subtitle;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+}
